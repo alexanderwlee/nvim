25020ea404437834773e3ed6b7b1acebb2044c4f
Alexander Lee <alexanderwlee@gmail.com>
2021-10-09 10:42:25 -0400

    Disable hint for lsp_signature

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -36,6 +36,7 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 require "lsp_signature".setup({
   hi_parameter = "IncSearch",
   hint_prefix = "",
+  hint_enable = false,
   bind = true,
   handler_opts = {
     border = "none"
