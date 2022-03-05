f37158202a4aeee6dd3712798729d471f1e2d160
Alexander Lee <alexanderwlee@gmail.com>
2022-03-05 15:56:54 -0500

    Move orgmode out of dropbox.

--- plugin/orgmode.lua
+++ plugin/orgmode.lua
@@ -1,5 +1,5 @@
 require('orgmode').setup({
-  org_agenda_files = {'~/Dropbox/org/*'},
+  org_agenda_files = {'~/orgmode/*'},
   notifications = {
     enabled = true,
     reminder_time = {1, 5, 10}
