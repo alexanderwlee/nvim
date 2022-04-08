868fa9fb3b10114c4abf5cfd0ddeea8f5f415072
Alexander Lee <alexanderwlee@gmail.com>
2022-04-07 20:29:04 -0400

    Use spaces instead of tabs for indents.

--- plugin/null-ls.lua
+++ plugin/null-ls.lua
@@ -5,39 +5,43 @@ local diagnostics = builtins.diagnostics
 local formatting = builtins.formatting
 
 local sources = {
-	-- Diagnostics
-	diagnostics.chktex.with({
-		-- Execute \input statements
-		extra_args = { "-I" },
-	}),
-	diagnostics.flake8,
-	diagnostics.mdl,
-	diagnostics.selene,
-	diagnostics.shellcheck,
+  -- Diagnostics
+  diagnostics.chktex.with({
+    -- Execute \input statements
+    extra_args = { "-I" },
+  }),
+  diagnostics.flake8,
+  diagnostics.mdl,
+  diagnostics.selene,
+  diagnostics.shellcheck,
 
-	-- Formatting
-	formatting.black,
-	formatting.google_java_format,
-	formatting.latexindent,
-	formatting.shfmt,
-	formatting.stylua,
-	formatting.trim_newlines,
-	formatting.trim_whitespace,
+  -- Formatting
+  formatting.black,
+  formatting.google_java_format,
+  formatting.shfmt.with({
+    -- Indent with 4 spaces
+    extra_args = { "-i", "4" },
+  }),
+  formatting.stylua.with({
+    extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+  }),
+  formatting.trim_newlines,
+  formatting.trim_whitespace,
 }
 
 -- Format on save
 local on_attach = function(client)
-	if client.resolved_capabilities.document_formatting then
-		vim.cmd([[
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd([[
       augroup LspFormatting
         autocmd! * <buffer>
         autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
       augroup END
     ]])
-	end
+  end
 end
 
 null_ls.setup({
-	sources = sources,
-	on_attach = on_attach,
+  sources = sources,
+  on_attach = on_attach,
 })
