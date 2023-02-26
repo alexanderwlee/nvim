6710a68b8aba30df1dbbe356b7ce43cc15ad3ce1
Alexander Lee <alexanderwlee@gmail.com>
2023-02-26 12:57:32 -0800

    Migrate to init.lua

---
+++ init.lua
@@ -0,0 +1,45 @@
+-- Settings
+vim.opt.clipboard = "unnamedplus" -- always use the clipboard for all operations
+vim.opt.completeopt = { "menu", "menuone", "noselect" } -- completion options
+vim.opt.expandtab = true -- replace tab with spaces
+vim.opt.hidden = true -- enable new buffer creation without saving previous buffer
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.showmode = false -- don't show mode on the last line
+vim.opt.number = true -- line numbers
+vim.opt.relativenumber = true -- set relative line numbers
+vim.opt.signcolumn = "yes" -- always draw the signcolumn
+vim.opt.smartcase = true -- override 'ignorecase' if the search pattern contains upper case characters
+vim.opt.termguicolors = true -- use GUI colors
+
+-- Leader
+vim.g.mapleader = " " -- use space as leader key
+
+-- Plugins
+require("packer").startup(function(use)
+  use("wbthomason/packer.nvim") -- package manager
+  use("SirVer/ultisnips") -- the ultimate snippets solution for vim
+  use("Yggdroot/indentLine") -- display the indention levels with thin vertical lines
+  use({ "bluz71/vim-moonfly-colors", as = "moonfly" }) -- a dark color scheme for vim
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  }) -- markdown preview plugin
+  use("jiangmiao/auto-pairs") -- insert or delete brackets, parens, quotes in pair
+  use({ "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" }) -- inject lsp diagnostics and code actions
+  use({ "nvim-tree/nvim-tree.lua", requires = "nvim-tree/nvim-web-devicons" }) -- a file explorer tree for neovim
+  use("lervag/vimtex") -- filetype plugin for LaTeX files
+  use("lewis6991/gitsigns.nvim") -- git integration for buffers
+  use("neovim/nvim-lspconfig") -- collection of configurations for built-in LSP client
+  use({ "nvim-lualine/lualine.nvim", requires = "kyazdani42/nvim-web-devicons" }) -- neovim statusline
+  use({ "nvim-telescope/telescope.nvim", requires = "nvim-lua/plenary.nvim" }) -- fuzzy finder over lists
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }) -- nvim treesitter configurations and abstraction layer
+  use("preservim/nerdcommenter") -- comment text
+  use("preservim/vim-markdown") -- markdown vim mode
+  use("sheerun/vim-polyglot") -- a solid language pack for vim
+  use("tpope/vim-fugitive") -- git plugin
+end)
+
+-- Color scheme
+vim.cmd([[colorscheme moonfly]])
