389645b5a0da04114875a5f89ccbc1b1a987c36d
Alexander Lee <alexanderwlee@gmail.com>
2024-04-11 22:48:46 -0400

    Enable treesitter incremental selection

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -3,4 +3,13 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<leader>gi", -- set to `false` to disable one of the mappings
+      node_incremental = "<leader>gn",
+      scope_incremental = "<leader>gs",
+      node_decremental = "<leader>gm",
+    },
+  },
 })
